a0a81f5fb2d220a8393d8b4586dd046398149b6a
Hugo Leitao <hygolei@gmail.com>
2021-07-21 09:36:38 +0100

    dev

--- zerver/webhooks/cofazure/view.py
+++ zerver/webhooks/cofazure/view.py
@@ -1,3 +1,38 @@
+# Webhooks for external integrations.
+from typing import Any, Dict, Iterable
+
+from django.http import HttpRequest, HttpResponse
+
+from zerver.decorator import api_key_only_webhook_view
+from zerver.lib.request import REQ, has_request_variables
+from zerver.lib.response import json_success
+from zerver.lib.webhooks.common import check_send_webhook_message
+from zerver.models import UserProfile
+
+
+@webhook_view('CofAzure')
+@has_request_variables
+def api_cofazure_webhook(
+        request: HttpRequest,
+	user_profile: UserProfile,
+        payload: Dict[str, Iterable[Dict[str, Any]]]=REQ(argument_type='body'),
+) -> HttpResponse:
+
+    # construct the body of the message
+    body = ''
+
+    # try to add the Wikipedia article of the day
+    body_template = 'Nova mensagem do azure : {detailedMessage}'
+    body +=  body_template.format(detailedMessage=payload['detailedMessage']['text'])
+
+    topic = payload['eventType']
+
+    # send the message
+    check_send_webhook_message(request, user_profile, topic, body)
+
+    return json_success()
+"""
+
 from typing import Any, Callable, Dict, Iterable
 
 from django.http import HttpRequest, HttpResponse
@@ -27,11 +62,12 @@ class Helper:
             unsupported_event=True,
         )
 
-
+"""
 """
 Mapeamento do eventtype devolvido pelo GIT
 A cada eventtype está a associada uma função que devolve o body e o topic da mensagem a publicar
 As funcoes encontram-se num outro ficheiro chamado bodyfunctions
+"""
 """
 EVENT_FUNCTION_MAPPER:Dict[str, Dict[str, Any]] ={
     "ms.vss-release.deployment-started-event": {"Function":br.release_deployment_started_body,
@@ -119,3 +155,4 @@ def api_cofazure_webhook(
         return json_success()
     except:
         return json_success()
+"""
