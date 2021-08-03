9107a5ff7b477c5b6cd469c969c38ad100364c37
hygolei <47327537+hygolei@users.noreply.github.com>
2021-08-03 14:41:59 +0100

    Update view.py

--- zerver/webhooks/cofazure/view.py
+++ zerver/webhooks/cofazure/view.py
@@ -8,7 +8,60 @@ from zerver.lib.request import REQ, has_request_variables
 from zerver.lib.response import json_success
 from zerver.lib.webhooks.common import check_send_webhook_message
 from zerver.models import UserProfile
+from zerver.lib.webhooks.bodyfunctions import BuildandRelease as br
+from zerver.lib.webhooks.bodyfunctions import code as cd
+from zerver.lib.webhooks.bodyfunctions import pipelines as pl
+from zerver.lib.webhooks.bodyfunctions import workitems as wk
 
+"""
+"""
+Mapeamento do eventtype devolvido pelo GIT
+A cada eventtype está a associada uma função que devolve o body e o topic da mensagem a publicar
+As funcoes encontram-se num outro ficheiro chamado bodyfunctions
+"""
+"""
+EVENT_FUNCTION_MAPPER:Dict[str, Dict[str, Any]] ={
+    "ms.vss-release.deployment-started-event": {"Function":br.release_deployment_started_body,
+                                                "Active":True},
+    "build.complete": {"Function": br.build_completed_body,
+                       "Active": True},
+    "ms.vss-release.release-abandoned-event": {"Function": br.release_abandoned_body,
+                                               "Active": True},
+    "ms.vss-release.release-created-event": {"Function": br.release_created_body,
+                                             "Active": True},
+    "ms.vss-release.deployment-approval-completed-event": {"Function": br.release_deployment_approval_completed_body,
+                                                           "Active": True},
+    "ms.vss-release.deployment-approval-pending-event": {"Function": br.release_deployment_approval_pending_body,
+                                                         "Active": True},
+    "git.pullrequest.merged": {"Function": cd.pull_request_merged_body,
+                               "Active": True},
+    "git.pullrequest.updated": {"Function": cd.pull_request_updated_body,
+                                "Active": True},
+    "tfvc.checkin": {"Function": cd.checkin_body,
+                     "Active": True},
+    "git.push": {"Function": cd.push_body,
+                 "Active": True},
+    "git.pullrequest.created": {"Function": cd.pull_request_created_body,
+                                "Active": True},
+    "ms.vss-pipelines.run-state-changed-event": {"Function": pl.run_state_changed_body,
+                                                 "Active": True},
+    "ms.vss-pipelinechecks-events.approval-completed": {"Function": pl.run_stage_approval_completed_body,
+                                                        "Active": True},
+    "ms.vss-pipelinechecks-events.approval-pending": {"Function": pl.run_stage_waiting_for_approval_body,
+                                                      "Active": True},
+    "ms.vss-pipelines.stage-state-changed-event": {"Function": pl.run_stage_state_changed_body,
+                                                   "Active": True},
+    "workitem.restored": {"Function": wk.work_item_restored_body,
+                          "Active": True},
+    "workitem.updated": {"Function": wk.work_item_updated_body,
+                         "Active": True},
+    "workitem.commented": {"Function": wk.work_item_commented_body,
+                           "Active": True},
+    "workitem.created": {"Function": wk.work_item_created_body,
+                         "Active": True},
+    "workitem.deleted": {"Function": wk.work_item_deleted_body,
+                         "Active": True},
+}
 
 @webhook_view('CofAzure')
 @has_request_variables
@@ -18,6 +71,16 @@ def api_cofazure_webhook(
         payload: Dict[str, Iterable[Dict[str, Any]]]=REQ(argument_type='body'),
 ) -> HttpResponse:
 
+    event = payload["eventType"]
+    
+    if event is None:
+            # Helper.log_unsupported(event)
+            return json_success()
+
+    # Retira a função a executrar da lista configurada mais acima
+    body_function = EVENT_FUNCTION_MAPPER[event]["Functions"]
+
+    
     # construct the body of the message
     body = ''
 
